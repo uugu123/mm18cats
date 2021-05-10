0f8b9f050bb808161b5b0d8c020edd6c96f287a5
Kaspar Suursalu <kasparsu@gmail.com>
2021-05-10 14:28:06 +0300

    initial commit

---
+++ database/factories/CatFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cat;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CatFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Cat::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $gender = $this->faker->randomElement(['MALE', 'FEMALE']);
+
+        return [
+            'name' => $this->faker->firstName(strtolower($gender)),
+            'gender' => $gender,
+            'birthday' => $this->faker->dateTimeThisDecade(),
+            'breed' => $this->faker->word(),
+            'description' => $this->faker->sentences(3,true),
+        ];
+    }
+}
---
+++ database/migrations/2021_05_03_132727_create_cats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('gender', ['MALE', 'FEMALE']);
+            $table->string('breed');
+            $table->dateTime('birthday');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+}
