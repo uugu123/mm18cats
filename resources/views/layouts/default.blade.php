3b74e2f9e1a35111c997058067502c0fe28ae23a
Kaspar Suursalu <kasparsu@gmail.com>
2021-05-10 16:16:16 +0300

    split views into different files

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.82.0">
+    <title>@yield('title')</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
+
+    <link href="{{mix("/css/app.css")}}" rel="stylesheet">
+</head>
+<body>
+
+@include('partials.header')
+
+<main>
+
+@yield('content')
+
+</main>
+
+@include('partials.footer')
+
+
+<script src="{{mix("/js/app.js")}}"></script>
+
+
+</body>
+</html>
